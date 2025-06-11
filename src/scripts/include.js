0eef46dc8a84211e1bfa8ed8b2b42bdd1ad2ad1c
ilustroc <jysus11092005@gmail.com>
2025-06-11 16:56:09 -0500

    Avance 3 - Version 2.0
    su

---
+++ src/scripts/include.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const includes = document.querySelectorAll("[data-include]");
+  includes.forEach(async el => {
+    const file = el.getAttribute("data-include");
+    try {
+      const response = await fetch(file);
+      if (!response.ok) throw new Error("Error al cargar: " + file);
+      const content = await response.text();
+      el.innerHTML = content;
+    } catch (err) {
+      el.innerHTML = `<p style="color:red;">${err.message}</p>`;
+    }
+  });
+});
